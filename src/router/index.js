2a446f6d95fd896b37eb97aae7916680df4bf507
Broghaml <56832888+Broghaml@users.noreply.github.com>
2019-11-18 16:03:52 +0100

    Layout

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Login from '../views/Login'
 import HelloWorld from "../components/HelloWorld";
 import Beispiel from "../views/Beispiel";
 import RegistrationProfessor from "../views/RegistrationProfessor";
+import Modulauswahl from "../views/Modulauswahl";
+import App from "../App";
+
 
 
 Vue.use(VueRouter)
@@ -11,8 +14,8 @@ Vue.use(VueRouter)
 export default new VueRouter({
     routes: [
         {
-            path: '/home',
-            component: HelloWorld
+            path: '/',
+            component: App
         },
         {
             path: '/login',
@@ -25,7 +28,13 @@ export default new VueRouter({
         {
             path: '/RegistrationProfessor',
             component: RegistrationProfessor
+        },
+        {
+            path: '/Modulauswahl',
+            component: Modulauswahl
         }
+
+
 
     ]
 })
