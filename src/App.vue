c178fd476a9aabe99fe2d70b21a1bd9e41ea9d16
Justin <justin.esposito@gmx.de>
2019-11-25 08:33:03 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-link to="/">Home</router-link>
+      <br>
+      <router-link to="/chat">Chat</router-link>
+      <br>
+      <router-link to="/kontaktformular">Kontaktformular</router-link>
+      <br>
+      <router-link to="/registrierung">Registrierung</router-link>
+      <br>
+      <router-link to="/module">Module</router-link>
+
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Kontaktformular.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Kontaktformular</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Kontaktformular'
+}
+</script>
+
+<style scoped>
+
+</style>
